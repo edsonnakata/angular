a3a654773f77c2a0e2d6b3bab2fba6ac9bc6daa8
enakata <edsonnakata2@gmail.com>
2021-03-29 11:31:16 -0400

    Event binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class DataBindingComponent implements OnInit {
 
   url: string = 'http://enaka.com'
+  textoSalvo: string = ""
+
+  onMouse: boolean = false;
 
   getValor(){
     return 1
@@ -15,8 +18,24 @@ export class DataBindingComponent implements OnInit {
 
   expressao: boolean = true
 
+  valorAtual: string = ""
+
   getTrue(){
     return true
+  }
+
+  botaoClicado(){alert("Botão foi clicado!!")}
+
+  onKeyUp(evento: KeyboardEvent){
+    console.log((<HTMLInputElement>evento.target).value)
+    this.valorAtual = (<HTMLInputElement>evento.target).value
+  }
+
+  salvaTexto(valor){
+    this.textoSalvo = valor
+  }
+  onMouseOverOut(){
+    this.onMouse = !this.onMouse
   }
 
   urlImagem = 'https://cdn.pixabay.com/photo/2020/08/07/13/16/plant-5470631_960_720.jpg'
