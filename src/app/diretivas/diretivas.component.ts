5e27ca77d6cbd41c3dc4ed18b0a25b788ad1fe89
enakata <edsonnakata2@gmail.com>
2021-03-30 11:10:43 -0400

    Diretiva *ngIf

---
+++ src/app/diretivas/diretivas.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretivas',
+  templateUrl: './diretivas.component.html',
+  styleUrls: ['./diretivas.component.scss']
+})
+export class DiretivasComponent implements OnInit {
+
+  cursos: string[] = ["Angular 2"]
+
+  mostrarCursos:boolean = false;
+
+  onMostrarCursos(){
+    this.mostrarCursos = !this.mostrarCursos
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
